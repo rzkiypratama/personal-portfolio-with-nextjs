5e409c62e97806cb6f2291914d5063e3a04a09c0
rzkiypratama <pratamabusiness@gmail.com>
2024-02-10 18:42:50 +0800

    feat: lol this is a feature?? 😂

---
+++ app/components/BackgroundCircle.tsx
@@ -0,0 +1,33 @@
+'use client'
+import React from "react";
+import { motion } from "framer-motion";
+import { Fragment } from "react";
+
+type Props = {};
+
+const BackgroundCircle: React.FC = () => {
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        scale: [1, 2, 2, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 1],
+        borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+      }}
+      transition={{
+        duration: 2.5,
+      }}
+      className="relative flex items-center justify-center"
+    >
+      <div className="absolute mt-52 h-[200px] w-[200px] animate-ping rounded-full border border-[#333]" />
+      <div className="absolute mt-52 h-[300px] w-[300px] rounded-full border border-[#333]" />
+      <div className="absolute mt-52 h-[500px] w-[500px] rounded-full border border-[#333]" />
+      <div className="absolute mt-52 h-[650px] w-[650px] rounded-full border border-[#f345] opacity-20" />
+      <div className="absolute mt-52 h-[800px] w-[800px] animate-pulse rounded-full border border-[#333]" />
+    </motion.div>
+  );
+}
+
+export default BackgroundCircle;
