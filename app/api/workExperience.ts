6cded09da182f38cc85ecb742ba2105bb6c87619
rzkiypratama <pratamabusiness@gmail.com>
2024-02-10 19:52:04 +0800

    static api for work experiences

---
+++ app/api/workExperience.ts
@@ -0,0 +1,62 @@
+export const workExperienceData = [
+    {
+      companyImages: "https://i.ibb.co/4fhXmwh/orion.jpg",
+      role: "Software Developer",
+      companyName: "Orion Scope",
+      skills: [
+        "https://i.ibb.co/Cvx0M9D/Vue-js-Logo-2-svg.png",
+        "https://i.ibb.co/mzsvQFP/Tailwind-CSS-Logo-svg.png",
+        "https://i.ibb.co/72WMfHp/Typescript-logo-2020-svg.png",
+        "https://www.pngrepo.com/png/354119/512/nodejs-icon.png",
+      ],
+      dateIn: "02 June 2023",
+      dateEnded: "",
+      currentlyWork: "Present",
+      summary: [
+        "Maintenance & made uph.edu faster & secure",
+        "Create waebajo.id from day1",
+        "Create crt.funedge.co.id Website",
+        "Create bfl-center.com Website",
+        "Work with code & no code environment",
+      ],
+    },
+    {
+      companyImages: "https://i.ibb.co/8D6FgPv/315365548-1332739860915960-254522410530346543-n.jpg",
+      role: "Software Engineer",
+      companyName: "Emori Creative",
+      skills: [
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--8sx8IYNE--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://i2.wp.com/blogreact.com/wp-content/uploads/2020/10/1_htbUdWgFQ3a94PMEvBr_hQ.png%3Fresize%3D750%252C428%26ssl%3D1",
+        "https://i.ibb.co/mzsvQFP/Tailwind-CSS-Logo-svg.png",
+        "https://www.svgrepo.com/download/12244/wordpress-logo.svg",
+        "https://i.ibb.co/72WMfHp/Typescript-logo-2020-svg.png",
+      ],
+      dateIn: "25 Des 2022",
+      dateEnded: "",
+      currentlyWork: "Present",
+      summary: [
+        "Build frontend & backend surabayasatu.co.id",
+        "Build a few landing page website.",
+        "Work with code & no code environment in development.",
+      ]
+    },
+    {
+      companyImages: "https://i.ibb.co/gSG8vN3/1663853843660.jpg",
+      role: "Frontend Developer",
+      companyName: "CV. Lucy Teknology",
+      skills: [
+        "https://i.ibb.co/Cvx0M9D/Vue-js-Logo-2-svg.png",
+        "https://i.ibb.co/mzsvQFP/Tailwind-CSS-Logo-svg.png",
+        "https://i.ibb.co/72WMfHp/Typescript-logo-2020-svg.png",
+        "https://www.pngrepo.com/png/354119/512/nodejs-icon.png",
+      ],
+      dateIn: "18 January 2022",
+      dateEnded: "30 April 2023",
+      currentlyWork: "",
+      summary: [
+        "Improve company website.",
+        "Improve company code.",
+        "Payment gateway integration.",
+      ]
+    },
+  ];
+  
